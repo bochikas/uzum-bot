538d7087906dced634b3ebbea3d29d9cbeae726f
bochikas <kam.damir@gmail.com>
2026-09-13 17:42:04 +0500

    f0195aadf351 migration fix

--- app/migration/versions/f0195aadf351_telegram_id_index.py
+++ app/migration/versions/f0195aadf351_telegram_id_index.py
@@ -8,6 +8,8 @@ Create Date: 2026-09-11 09:48:23.759484
 
 from typing import Sequence, Union
 
+from alembic import op
+
 # revision identifiers, used by Alembic.
 revision: str = "f0195aadf351"
 down_revision: Union[str, None] = "f06c6588eba0"
@@ -18,6 +20,10 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     """Upgrade schema."""
 
+    op.create_index(op.f("ix_users_telegram_id"), "users", ["telegram_id"], unique=True)
+
 
 def downgrade() -> None:
     """Downgrade schema."""
+
+    op.drop_index(op.f("ix_users_telegram_id"), table_name="users")
